7dc755005ceb2fe9cb1d05df24c3f6681c05a4ba
nimaxin <nazirnasiri786@gmail.com>
2026-02-18 03:46:01 +0330

    test: Add initial test cases for allocation functionality

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
-def main():
-    print("Hello from architecture-patterns-fastapi!")
+import pytest
 
 
-if __name__ == "__main__":
-    main()
+def test_allocating_to_a_batch_reduces_the_available_quantity():
+    pytest.fail()
+
+
+def test_can_allocate_if_available_greater_than_required():
+    pytest.fail()
+
+
+def test_cannot_allocate_if_available_smaller_than_required():
+    pytest.fail()
+
+
+def test_can_allocate_if_available_equal_to_required():
+    pytest.fail()
+
+
+def test_cannot_allocate_if_skus_do_not_match():
+    pytest.fail()
+
+
+def test_can_only_deallocate_allocated_lines():
+    pytest.fail()
+
+
+def test_allocation_is_idempotent():
+    pytest.fail()
+
+
+def test_prefers_current_stock_batches_to_shipments():
+    pytest.fail()
+
+
+def test_prefers_earlier_batches():
+    pytest.fail()
+
+
+def test_returns_allocated_batch_ref():
+    pytest.fail()
+
+
+def test_raises_out_of_stock_exception_if_cannot_allocate():
+    pytest.fail()
