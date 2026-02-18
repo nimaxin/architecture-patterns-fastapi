7774f6f7fe14a76d08408f854019a76b14d0784b
nimaxin <nazirnasiri786@gmail.com>
2026-02-18 04:01:32 +0330

    feat: Implement OrderLine and Batch classes with allocation logic

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
+from dataclasses import dataclass
+
 import pytest
 
 
+@dataclass(frozen=True)
+class OrderLine:
+    sku: str
+    quantity: int
+
+
+class Order:
+    def __init__(self, id: int, lines: list[OrderLine]):
+        self.id = id
+        self.lines = lines
+
+
+class Batch:
+    def __init__(self, id: int, sku: str, quantity):
+        self.id = id
+        self.sku = sku
+        self.quantity = quantity
+        self._allocated_order_lines: set[OrderLine] = set()
+
+    def allocate(self, order_line: OrderLine):
+        self._allocated_order_lines.add(order_line)
+
+    @property
+    def available_quantity(self) -> int:
+        return self.quantity - sum(
+            order_lines.quantity for order_lines in self._allocated_order_lines
+        )
+
+
 def test_allocating_to_a_batch_reduces_the_available_quantity():
-    pytest.fail()
+    batch = Batch(1, "chair", 10)
+    order_line = OrderLine("chair", 5)
+    batch.allocate(order_line)
+    assert batch.available_quantity == 5
 
 
 def test_can_allocate_if_available_greater_than_required():
