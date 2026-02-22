e7fe040716fe93b19a5865b9fab48efd652f66c8
nimaxin <nazirnasiri786@gmail.com>
2026-02-22 03:31:44 +0330

    test: add add_batch tests

--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -2,7 +2,7 @@ import pytest
 
 from app.allocation import services
 from app.allocation.adapters.repository import FakeRepository
-from app.allocation.domain import Batch, OrderLine
+from app.allocation.domain import Batch
 from app.allocation.domain.errors import OutOfStockError
 
 
@@ -12,20 +12,24 @@ class FakeSession:
 
 
 async def test_returns_allocation():
-    order_line = OrderLine("chair", 1, "order-id")
     batch = Batch("batch-ref", "chair", 10)
     repository = FakeRepository([batch])
     session = FakeSession()
 
-    batch_ref = await services.allocate(order_line, repository, session)  # type: ignore
+    batch_ref = await services.allocate("chair", 1, "order-id", repository, session)  # type: ignore
     assert batch_ref == batch.reference
 
 
 async def test_error_for_invalid_sku():
-    order_line = OrderLine("unknown", 1, "order-id")
     batch = Batch("batch-ref", "chair", 10)
     repository = FakeRepository([batch])
     session = FakeSession()
 
     with pytest.raises(OutOfStockError):
-        await services.allocate(order_line, repository, session)  # type: ignore
+        await services.allocate("unknown", 1, "order-id", repository, session)  # type: ignore
+
+
+async def test_add_batch():
+    repository = FakeRepository([])
+    await services.add_batch("batch-ref", "sku", 1, None, repository)
+    assert await repository.get("batch-ref") is not None
